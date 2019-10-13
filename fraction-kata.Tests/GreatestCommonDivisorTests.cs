c73f739e9be119bf1b29f817c2d29b092b40d2e5
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 18:01:08 +0200

    tests of gcd of numbers with no common factors

--- fraction-kata.Tests/GreatestCommonDivisorTests.cs
+++ fraction-kata.Tests/GreatestCommonDivisorTests.cs
@@ -12,6 +12,14 @@ namespace fraction_kata.Tests
             Assert.AreEqual(-1, Gcd(-1, -1));
         }
 
+        [Test]
+        public void NumbersWithNoCommonFactors()
+        {
+            Assert.AreEqual(1, Gcd(2, 5));
+            Assert.AreEqual(1, Gcd(3, 7));
+            Assert.AreEqual(-1, Gcd(-2, -3));
+        }
+
         private int Gcd(int a, int b)
         {
             while (b != 0)
