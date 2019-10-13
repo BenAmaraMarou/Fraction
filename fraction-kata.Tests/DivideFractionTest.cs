f6abe0ebd761b496b595f53eb0f7af695503976d
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 21:26:13 +0200

    pass test divide zero

---
+++ fraction-kata.Tests/DivideFractionTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace fraction_kata.Tests
+{
+    public class DivideFractionTest
+    {
+        [Test]
+        public void ZeroNumerator()
+        {
+            Assert.AreEqual(new Fraction(0), new Fraction(0).Divide(new Fraction(5)));
+        }
+    }
+}
