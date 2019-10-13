f9be786915236dff4c24c30efd410becfabac3cc
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 12:52:31 +0200

    pass test add 0+0

--- fraction-kata.Tests/FractionTests.cs
+++ fraction-kata.Tests/FractionTests.cs
@@ -5,10 +5,24 @@ namespace fraction_kata.Tests
     public class FractionTests
     {
         [Test]
-        public void Should_fail()
+        public void AddZeroToZero()
         {
-            Assert.Fail("Make me pass");
+            Assert.AreEqual(0, new Fraction(0).Add(new Fraction(0)));
         }
 
+        private class Fraction
+        {
+            private readonly int _number;
+
+            public Fraction(int number)
+            {
+                this._number = number;
+            }
+
+            internal int Add(Fraction fraction)
+            {
+                return 0;
+            }
+        }
     }
 }
