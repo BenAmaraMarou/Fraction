cb4db221eefa732964ec44938f37374341243e8c
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 17:54:54 +0200

    copy gcd algorithm from wikipedia

---
+++ fraction-kata.Tests/GreatestCommonDivisorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace fraction_kata.Tests
+{
+    public class GreatestCommonDivisorTests
+    {
+        [Test]
+        public void GcdOfOneAndOneIsOne()
+        {
+            Assert.AreEqual(1, Gcd(1, 1));
+        }
+
+        internal int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+    }
+}
