d8884a7867771c433d6a2cb0b99122a19707c82b
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 21:27:45 +0200

    divide zero by any integer

--- fraction-kata.Tests/DivideFractionTest.cs
+++ fraction-kata.Tests/DivideFractionTest.cs
@@ -4,10 +4,12 @@ namespace fraction_kata.Tests
 {
     public class DivideFractionTest
     {
-        [Test]
-        public void ZeroNumerator()
+        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(5)]
+        public void ZeroNumerator(int nonZero)
         {
-            Assert.AreEqual(new Fraction(0), new Fraction(0).Divide(new Fraction(5)));
+            Assert.AreEqual(new Fraction(0), new Fraction(0).Divide(new Fraction(nonZero)));
         }
     }
 }
