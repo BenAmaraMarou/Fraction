5279554a5b78fd8abcf9ac0bb6979d0e0e176a48
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 18:48:04 +0200

    inline tests

--- fraction-kata.Tests/FractionTest.cs
+++ fraction-kata.Tests/FractionTest.cs
@@ -9,8 +9,7 @@ namespace fraction_kata.Tests
         [TestCase(-1)]
         public void AddZeroToAny(int nonZero)
         {
-            Fraction actual = new Fraction(nonZero).Add(new Fraction(0));
-            Assert.AreEqual(new Fraction(nonZero), actual);
+            Assert.AreEqual(new Fraction(nonZero), new Fraction(nonZero).Add(new Fraction(0)));
         }
 
         [TestCase(1, 2)]
@@ -19,51 +18,44 @@ namespace fraction_kata.Tests
         [TestCase(-1, -2)]
         public void AddNonZeroIntegers(int first, int second)
         {
-            Fraction actual = new Fraction(first).Add(new Fraction(second));
-            Assert.AreEqual(new Fraction(first + second), actual);
+            Assert.AreEqual(new Fraction(first + second), new Fraction(first).Add(new Fraction(second)));
         }
 
         [Test]
         public void AddFractionsWithSameDenominator()
         {
-            Fraction actual = new Fraction(3, 7).Add(new Fraction(1, 7));
-            Assert.AreEqual(new Fraction(4, 7), actual);
+            Assert.AreEqual(new Fraction(4, 7), new Fraction(3, 7).Add(new Fraction(1, 7)));
         }
 
         [Test]
         public void AddFractionsWithDifferentDenominators()
         {
-            Fraction actual = new Fraction(1, 2).Add(new Fraction(1, 3));
-            Assert.AreEqual(new Fraction(5, 6), actual);
+            Assert.AreEqual(new Fraction(5, 6), new Fraction(1, 2).Add(new Fraction(1, 3)));
         }
 
         [TestCase(-1, 2)]
         [TestCase(1, -2)]
         public void AddNegativeFractions(int numerator, int denominator)
         {
-            Fraction actual = new Fraction(numerator, denominator).Add(new Fraction(-1, 4));
-            Assert.AreEqual(new Fraction(-6, 8), actual);
+            Assert.AreEqual(new Fraction(-6, 8), new Fraction(numerator, denominator).Add(new Fraction(-1, 4)));
         }
         
         [Test]
         public void ReduceAddResult()
         {
-            Fraction actual = new Fraction(1, 4).Add(new Fraction(1, 4));
-            Assert.AreEqual(new Fraction(1, 2), actual);
+            Assert.AreEqual(new Fraction(1, 2), new Fraction(1, 4).Add(new Fraction(1, 4)));
         }
 
         [Test]
         public void ReduceAddResultToOne()
         {
-            Fraction actual = new Fraction(1, 2).Add(new Fraction(1, 2));
-            Assert.AreEqual(new Fraction(1), actual);
+            Assert.AreEqual(new Fraction(1), new Fraction(1, 2).Add(new Fraction(1, 2)));
         }
 
         [Test]
         public void ReduceResultOfFractionsWithMultipleDenominators()
         {
-            Fraction actual = new Fraction(3, 4).Add(new Fraction(5, 8));
-            Assert.AreEqual(new Fraction(11, 8), actual);
+            Assert.AreEqual(new Fraction(11, 8), new Fraction(3, 4).Add(new Fraction(5, 8)));
         }
     }
 }
