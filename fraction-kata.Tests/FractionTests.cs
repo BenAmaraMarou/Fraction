1416310db5c8dff6ce56402957b3ad044ffb234a
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 15:25:45 +0200

    replace getters with implemented equals

--- fraction-kata.Tests/FractionTests.cs
+++ fraction-kata.Tests/FractionTests.cs
@@ -11,8 +11,6 @@ namespace fraction_kata.Tests
         public void AddZeroToAny(int nonZero)
         {
             Fraction actual = new Fraction(nonZero).Add(new Fraction(0));
-            Assert.AreEqual(nonZero, actual.Numerator());
-            Assert.AreEqual(1, actual.Denominator());
             Assert.AreEqual(new Fraction(nonZero), actual);
         }
 
@@ -20,56 +18,49 @@ namespace fraction_kata.Tests
         public void AddPositiveIntegers()
         {
             Fraction actual = new Fraction(1).Add(new Fraction(2));
-            Assert.AreEqual(3, actual.Numerator());
-            Assert.AreEqual(1, actual.Denominator());
+            Assert.AreEqual(new Fraction(3), actual);
         }
 
         [Test]
         public void AddNegativeIntegers()
         {
             Fraction actual = new Fraction(-1).Add(new Fraction(-2));
-            Assert.AreEqual(-3, actual.Numerator());
-            Assert.AreEqual(1, actual.Denominator());
+            Assert.AreEqual(new Fraction(-3), actual);
         }
 
         [Test]
         public void AddFractionsWithSameDenominator()
         {
             Fraction actual = new Fraction(3, 7).Add(new Fraction(1, 7));
-            Assert.AreEqual(4, actual.Numerator());
-            Assert.AreEqual(7, actual.Denominator());
+            Assert.AreEqual(new Fraction(4, 7), actual);
         }
 
         [Test]
         public void AddFractionsWithDifferentDenominators()
         {
             Fraction actual = new Fraction(1, 2).Add(new Fraction(1, 3));
-            Assert.AreEqual(5, actual.Numerator());
-            Assert.AreEqual(6, actual.Denominator());
+            Assert.AreEqual(new Fraction(5, 6), actual);
         }
 
         [Test]
         public void AddNegativeFractionsOnNumerator()
         {
             Fraction actual = new Fraction(-1, 2).Add(new Fraction(-1, 4));
-            Assert.AreEqual(-6, actual.Numerator());
-            Assert.AreEqual(8, actual.Denominator());
+            Assert.AreEqual(new Fraction(-6, 8), actual);
         }
 
         [Test]
         public void AddNegativeFractionsOnDenominator()
         {
             Fraction actual = new Fraction(1, -2).Add(new Fraction(-1, 4));
-            Assert.AreEqual(-6, actual.Numerator());
-            Assert.AreEqual(8, actual.Denominator());
+            Assert.AreEqual(new Fraction(-6, 8), actual);
         }
 
         [Test]
         public void AddFractionsWithSameNegativeDenominator()
         {
             Fraction actual = new Fraction(1, -7).Add(new Fraction(2, -7));
-            Assert.AreEqual(-3, actual.Numerator());
-            Assert.AreEqual(7, actual.Denominator());
+            Assert.AreEqual(new Fraction(-3, 7), actual);
         }
 
         private class Fraction : IEquatable<Fraction>
@@ -94,16 +85,6 @@ namespace fraction_kata.Tests
                     return new Fraction(_numerator * fraction._denominator + _denominator * fraction._numerator, _denominator * fraction._denominator);
                 }
                 return new Fraction(_numerator + fraction._numerator, _denominator);
-            }
-
-            internal int Denominator()
-            {
-                return _denominator;
-            }
-
-            internal int Numerator()
-            {
-                return _numerator;
             }
 
             public bool Equals(Fraction other)
