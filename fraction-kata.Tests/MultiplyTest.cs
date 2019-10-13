8906883d3e74cebb8ef4225ef6194faf3c45f392
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 20:57:52 +0200

    pass test multiply 0

---
+++ fraction-kata.Tests/MultiplyTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace fraction_kata.Tests
+{
+    public class MultiplyTest
+    {
+
+        [Test]
+        public void MultiplyZero()
+        {
+            Assert.AreEqual(new Fraction(0), new Fraction(5).Multiply(new Fraction(0)));
+        }
+
+    }
+}
