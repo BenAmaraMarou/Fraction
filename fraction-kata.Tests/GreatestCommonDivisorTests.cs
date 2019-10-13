87e94d2ab733ec6dd973f7760071860d3cc1c139
Marouane BEN AMARA <marouane.benamara@arolla.fr>
2019-10-13 17:58:00 +0200

    gather samples of gcd of same numbers

--- fraction-kata.Tests/GreatestCommonDivisorTests.cs
+++ fraction-kata.Tests/GreatestCommonDivisorTests.cs
@@ -5,12 +5,14 @@ namespace fraction_kata.Tests
     public class GreatestCommonDivisorTests
     {
         [Test]
-        public void GcdOfOneAndOneIsOne()
+        public void SameNumbers()
         {
             Assert.AreEqual(1, Gcd(1, 1));
+            Assert.AreEqual(2, Gcd(2, 2));
+            Assert.AreEqual(-1, Gcd(-1, -1));
         }
 
-        internal int Gcd(int a, int b)
+        private int Gcd(int a, int b)
         {
             while (b != 0)
             {
